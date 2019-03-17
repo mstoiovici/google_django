8906179d17d2bcf1ed90299cf92a95a35c2dc26b
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2019-03-17 20:28:42 +0000

    added item model

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Item(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
